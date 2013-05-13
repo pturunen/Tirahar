4cdf5b7b3c5e3ece671c6dd72800ecb4f44fc951
pturunen <pturunen@ovi.com>
2013-05-14 01:03:55 +0300

    lisätty default rungot

---
+++ NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Binomikeko.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kekoharjoitus;
+
+/**
+ *
+ * @author pturunen
+ */
+public class Binomikeko {
+    
+    Binomikeko(){
+        
+    }
+    
+    /*
+     * 
+     */
+    public void makeHeap(){
+       /* to be implemented*/ 
+    }
+    
+    /*
+     * return value integer or object??
+     */
+    public int findMin(/* Heap*/){
+        /* to be implemented*/
+        return 0;
+    }
+    
+    /*
+     * input parameter x ;integer or object??
+     */
+    public void insert(/* heap, int x*/){
+        /* to be implemented*/
+    }
+    
+    /*
+     * return value integer or object??
+     */
+    public int deleteMin(/* heap */){
+         /* to be implemented*/
+        return 0;
+    }
+    
+    /*
+     * 
+     */
+    public void decreaseKey(){
+        /* to be implemented*/ 
+    }
+    
+    /*
+     * 
+     */
+    public void merge(/*heap1, heap2*/){
+        /* to be implemented*/ 
+    }
+}
