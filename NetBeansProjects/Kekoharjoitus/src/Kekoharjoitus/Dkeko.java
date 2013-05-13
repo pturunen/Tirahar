f7203688a7f36008be23880973b6f28382386e7e
pturunen <pturunen@ovi.com>
2013-05-14 02:18:36 +0300

    muokattu kommentteja

--- NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Dkeko.java
+++ NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Dkeko.java
@@ -15,14 +15,14 @@ public class Dkeko {
     }
     
     /**
-     *
+     * Luo uuden tyhjän keon
      */
     public void makeHeap(){
         
     }
     
     /**
-     *
+     * Palauttaa osoittimen keon pienimpään avaimeen
      * @return
      */
     public int findMin(/* Heap*/){
@@ -31,14 +31,14 @@ public class Dkeko {
     }
     
     /**
-     * input parameter x ;integer or object??
+     * Lisää alkion x, jonka avain on key[x] kekoon
      */
     public void insert(/* heap, int x*/){
         /* to be implemented*/
     }
     
     /**
-     *
+     * Poistaa pienimmän alkion keosta ja palauttaa osoittimen ko alkioon
      * @return
      */
     public int deleteMin(/* heap */){
@@ -47,14 +47,15 @@ public class Dkeko {
     }
     
     /**
-     *
+     * Muuttaa keossa olevan alkion x arvoa pienemmäksi arvoon k
      */
-    public void decreaseKey(){
+    public void decreaseKey(/* x,k*/){
         /* to be implemented*/ 
     }
     
     /**
-     *
+     * Yhdistää kaksi kekoa toisiinsa,luoden uuden keon ja
+     * tuhoten keon T1 ja T2
      */
     public void merge(/*heap1, heap2*/){
         /* to be implemented*/ 
