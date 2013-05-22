a48919b673deae31d31cda5766c784e2d34a4c04
pturunen <pturunen@ovi.com>
2013-05-22 13:58:08 +0300

    muokattu decreaseKey ja heap-del-min funktioita

--- NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Dkeko.java
+++ NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Dkeko.java
@@ -18,6 +18,10 @@ public class Dkeko {
     final int error = -1;
     final int ok = 0;
     
+    /**
+     * Dkeon konstruktori
+     * @param k alustaa d keon haarautumisasteen
+     */
     Dkeko(int k){
         this.d = k;
     }
@@ -26,8 +30,8 @@ public class Dkeko {
      * Luo uuden tyhjän keon
      */
     public void makeHeap(){
-        //Konstruktori luo keon,
-        //mikä on tämän funktion lisäarvo ?
+        //todo:tarkista voiko tämä muodostaa taulukosta
+        //Solmu olioita minheapin.
         heapSize = 0;
         min = null;
         tail = min;
@@ -70,22 +74,74 @@ public class Dkeko {
     public Solmu deleteMin(){
          /* to be implemented*/
         /* delete and make heapify*/
-        return min;
+        Solmu mini = min;
+        if (heapSize > 0){
+           min = tail;
+           min.setLeft(mini.getLeft());
+           min.setRight(mini.getRight());
+           min.setKey(mini.getKey());
+           tail = tail.getLeft();
+           heapSize = heapSize -1;
+           minHeapify(min);
+        }
+        return mini;
     }
     
+    
+    private void minHeapify(Solmu solmu){
+        //todo:lapsia d maara 1 to d
+        if(heapSize == 0 || heapSize ==1){
+            return;
+        }
+        Solmu lapsi = null;
+        Solmu pienin = solmu;
+        for (int i=1;i<= d;i++){
+            lapsi = annaLapsi(solmu,i);
+            if (lapsi!=null){
+                if (lapsi.getKey() <= heapSize && lapsi.getValue() < pienin.getValue()){
+                    pienin = lapsi;
+                }
+            }
+        }
+        if (pienin != solmu){
+            //vaihdetaan solmujen paikkaa
+            solmu = muutaOsoittimet(pienin,solmu);
+            minHeapify(solmu);
+        }
+    }
+    
+    /*
+     * dkeon lapset i = parent,
+     * id +1,id+2,..,id +d
+     */
+    private Solmu annaLapsi(Solmu solmu,int monesko){
+        Solmu lapsi = null;
+        if (monesko > d || monesko == 0){
+            return lapsi;
+        }
+        int lapsikey = (solmu.getKey()*d)+monesko;
+        if (lapsikey < heapSize){
+            lapsi = findSolmu(lapsikey);
+        }
+        return lapsi;
+    }
+    
+    
     /**
-     * Muuttaa keossa olevan solmun arvoa ja
-     * paikkaa keossa, jos tarve vaatii
+     * Pienentää keossa olevan solmun arvoa ja
+     * muuttaa solmun paikkaa keossa ylöspäin, jos tarve vaatii
      * @param x 
      * @param value 
      * @return 
      */
     public int decreaseKey(Solmu x,int value){
         /* to be implemented*/ 
-        //todo:nyt toteutus ylöspäin,po myös alaspäin
         int res = error;
-        if(x.getKey() < 0 || x.getKey() >= heapSize){
+        if (x.getKey() < 0 || x.getKey() >= heapSize){
             return res;
+        }
+        if (x.getValue() < value){
+            return error;
         }
         x.setValue(value);
         res = vaihdaJarjestys(x);
@@ -102,15 +158,15 @@ public class Dkeko {
      */
     private int vaihdaJarjestys(Solmu lapsi){
         int res=0;
-        if(lapsi != null && lapsi == min && lapsi.getKey()==min.getKey() ){
-            /* oletetaan että solmu kuuluu kekoon ,validi key arvo*/
-            /* todo:tarkista onko perusteltu olettamus*/
+        
+        if (lapsi != null && lapsi == min ){
             return res;
         }
+        
         Solmu solmu;
         int p =  countParent(lapsi.getKey());
         solmu = findSolmu(p);
-        if(solmu == null ){
+        if (solmu == null ){
             return error;  
         }
         if (solmu.getValue() > lapsi.getValue()){
@@ -124,7 +180,7 @@ public class Dkeko {
      * Vaihdetaan Solmun paikkaa keossa,left ja right 
      *  linkit päivitetään, sekä key arvo
      * @param s1 
-     * @param s2 
+     * @param s2 keossa kohteena oleva solmu
      * @return null, jos s1 tai s2 ovat null,muutoin s2 olio
      */
     private Solmu muutaOsoittimet(Solmu s1, Solmu s2){
@@ -132,22 +188,16 @@ public class Dkeko {
         //turha ja epaselva tarkastella pitkin matkaa!
         Solmu solmuL;
         Solmu solmuR;
-        if(s1 == null || s2 == null){
+        if (s1 == null || s2 == null){
             return null;
         }
         int apu;
         apu = s1.getKey();
         s1.setKey(s2.getKey());
         s2.setKey(apu);
-        if(s2 == tail){
-            tail = s1;
-        }
-        if(s1 == min){
-            min=s2;
-        }
-        
+        updateMinTail(s1,s2);
         solmuL = s1.getLeft();
-        if(solmuL != null){
+        if (solmuL != null){
             solmuL.setRight(s2);
         }
         
@@ -155,20 +205,33 @@ public class Dkeko {
         s2.getLeft().setRight(s1);
         s2.setLeft(solmuL);
         solmuR = s1.getRight();
-        if(solmuR != null){
+        if (solmuR != null){
             solmuR.setLeft(s2);
         }
         
         s1.setRight(s2.getRight());
         s2.setRight(solmuR);
-        if(s1.getRight()!=null){
+        if (s1.getRight()!=null){
             s1.getRight().setLeft(s1);
         }
-        
-        
         return s2;
     }
     
+    
+    private void updateMinTail(Solmu s1,Solmu s2){
+        if (s2 == tail){
+            tail = s1;
+        }
+        if (s1 == min){
+            min=s2;
+        }
+        if (s1 == tail){
+            s2 = tail;
+        }
+        if (s2 == min){
+            min = s1;
+        }
+    }
     
     /**
      *
@@ -177,7 +240,7 @@ public class Dkeko {
      */
     private int countParent(int child){
         int p = error;
-        if(child ==0){
+        if (child ==0){
             return p;
         }
         p = (child-1)/d;
@@ -191,9 +254,9 @@ public class Dkeko {
      */
     private Solmu findSolmu(int key) {
         Solmu solmu = min;
-        if(min!=null){
-            for(int i=min.getKey();i<heapSize;i++){
-                if(solmu.getKey()== key){
+        if (min!=null){
+            for (int i=min.getKey();i<heapSize;i++){
+                if (solmu.getKey()== key){
                     return solmu;
                 }
                 solmu = solmu.getRight();
