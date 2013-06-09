41ac2ee2125ac3bbfbfe419723841556a4dfbd74
pturunen <pturunen@ovi.com>
2013-06-09 21:36:36 +0300

    välitalletus ,deletemin kesken

--- NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Fibonaccikeko.java
+++ NetBeansProjects/Kekoharjoitus/src/Kekoharjoitus/Fibonaccikeko.java
@@ -49,8 +49,8 @@ public class Fibonaccikeko {
        if (x == null || x.getValue() == Integer.MAX_VALUE){
             return;
         }
-     Fibonaccipuu uusi = createNewFibonaccipuu(x);
-     if (uusi != null){
+        Fibonaccipuu uusi = createNewFibonaccipuu(x);
+        if (uusi != null){
             if (this.getMin()!=null)  {
                 Fibonaccikeko keko = this.makeHeap();
                 keko.setMin(uusi);
@@ -59,9 +59,8 @@ public class Fibonaccikeko {
             }
             else {
                 this.setMin(uusi);
-         }
-     } 
-  
+            }
+        } 
     }
   
     /**
@@ -78,7 +77,7 @@ public class Fibonaccikeko {
      * Asettaa Fibonaccikeko olion jäsenmuuttujaan
      * min parametrina annetun Fibonaccipuu olion
      * 
-     * @param fibonaccipuu Fibonacipuu olio
+     * @param fibonaccipuu Fibonaccipuu olio
      */
     private void setMin(Fibonaccipuu fibonaccipuu){
         this.min =fibonaccipuu;
@@ -97,12 +96,100 @@ public class Fibonaccikeko {
     }
     
     /**
-     *
-     * @return
+     * Poistaa pienimmän alkion keosta ja palauttaa osoittimen ko alkioon
+     * Fibonaccikeon poisto toimii seuraavasti;
+     * poistetaan ensin minimialkio juurilistasta ja sen mahdollinen
+     * lapsilista yhdistetaan juurilistaan,Paivitetaan parent linkit ja
+     * yhdistetaan saman degree arvon omaavat juurilistan alkiot toisiinsa.
+     * @return Solmu luokan olio, poistetaan keosta
      */
-    public int deleteMin(/* heap */){
-         /* to be implemented*/
-        return 0;
+    public Solmu deleteMin(){
+        if (this.getMin()== null){
+            return null;
+        }
+        Fibonaccipuu minimi = this.getMin();
+        this.setMin(getUusiMinimi());
+        removeMin();
+        this.nostaLapsetJuurilistaan(minimi.getChild());
+        //todo yhdista samanasteiset
+        
+        return minimi.getValue();
+    }
+    
+    /**
+     * Poistaa pienimmän alkion juurilistasta ja
+     * päivittää vanhan minimin vasemman ja oikean alkion
+     * linkit juurilistassa
+     */
+    private void removeMin(){
+        if (this.getMin()== null){
+            return;
+        }
+        Fibonaccipuu  vasen = this.getMin().getSiblingL();
+        Fibonaccipuu  oikea = this.getMin().getSiblingR();
+        if (vasen != null && vasen != oikea){
+            vasen.setSiblingR(oikea);
+            oikea.setSiblingL(vasen);
+        }
+        else if (vasen!=null){
+            vasen.setSiblingL(null);
+            vasen.setSiblingR(null);
+        }
+    }
+    
+    /**
+     * Etsii uuden minimiarvon juurilistasta ja vanhan minimin lapsilistan
+     * joukosta, päivittää vanhan minimin lapsilistan alkioiden parent = null
+     * @return Fibonaccipuu
+     */
+    private Fibonaccipuu getUusiMinimi(){
+        Fibonaccipuu uusi = null;
+        uusi = this.getMin().getSiblingR();
+        Fibonaccipuu ehdokas = uusi;
+        while (this.getMin()!= ehdokas && ehdokas !=null){
+            if (uusi.getValue().getValue() > 
+                    ehdokas.getValue().getValue()){
+                uusi = ehdokas;
+            }
+            ehdokas = ehdokas.getSiblingR();
+        }
+        ehdokas = this.getMin().getChild();
+        if (uusi == null){
+            uusi = ehdokas;
+        }
+        while (this.getMin().getChild() != ehdokas && ehdokas != null){
+            if (uusi.getValue().getValue() > 
+                    ehdokas.getValue().getValue()){
+                uusi = ehdokas;
+            }
+            ehdokas.setParent(null);
+            ehdokas= ehdokas.getSiblingR();
+        }
+        return uusi;
+    }
+    
+    /**
+     * Nostaa lapsilistan alkiot juurilistaan
+     * Muodostaa ensin uuden keon ja yhdistaa sen vanhaan kekoon
+     * @param lapsi juurilistaan nostettavan lapsilistan ensimmainen alkio
+     */
+    private void nostaLapsetJuurilistaan(Fibonaccipuu lapsi){
+        if (lapsi == null){
+            return;
+        }
+        Fibonaccikeko uusikeko = Fibonaccikeko.makeHeap();
+        Fibonaccipuu pienin = lapsi;
+        Fibonaccipuu ehdokas = lapsi.getSiblingR();
+        while (lapsi != ehdokas && ehdokas != null){
+            if (pienin.getValue().getValue() > 
+                    ehdokas.getValue().getValue()){
+                pienin = ehdokas;
+            }
+            ehdokas= ehdokas.getSiblingR();
+        }
+        uusikeko.setMin(pienin);
+        Fibonaccikeko temp = Fibonaccikeko.merge(uusikeko, this);
+        this.setMin(temp.getMin());
     }
     
     /**
@@ -134,13 +221,11 @@ public class Fibonaccikeko {
         Fibonaccipuu suurempi = null;
         keko = Fibonaccikeko.makeHeap();
         Fibonaccipuu uusiMin = null;
+        keko.setMin(keko2.getMin());
+        suurempi = keko1.getMin();
         if (keko1.findMin().getValue() <= keko2.findMin().getValue()){
                 keko.setMin(keko1.getMin());
                 suurempi = keko2.getMin();
-            }
-        else {
-                keko.setMin(keko2.getMin());
-                suurempi = keko1.getMin();
             }
         uusiMin = keko.getMin();
         Fibonaccipuu keko1vasen = keko1.getMin().getSiblingL();
@@ -165,7 +250,6 @@ public class Fibonaccikeko {
         }
         return keko;        
     }
-    
     
     private static void asetaLinkit(Fibonaccipuu vasen, Fibonaccipuu oikea){
         if (vasen == null || oikea == null){
